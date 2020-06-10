8e3dd4a00450fea6f190d1ac7c796fcbe19e1d6d
Justin Carter <justincarter@Hannahs-MacBook-Air.local>
2020-06-09 19:01:25 -0500

    More changes

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!!!'
+
+@app.route('/hello/<name>')
+def show_user_profile(name):
+    # show the user profile for that user
+    return 'Hello, %s!' % escape(name)
+
+@app.route('/test1')
+def test():
+    return 'Hello, test!!!'
+
+with app.test_request_context():
+    print(url_for('show_user_profile', name='Justin'))
